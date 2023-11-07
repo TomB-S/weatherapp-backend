771965fd6c8f3ed2d43c6aabc168a0deadd3c341
TomB-S <147733386+TomB-S@users.noreply.github.com>
2023-11-07 09:00:55 +0100

    test backend

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://vercel.com/totos-projects-883b9d76/weatherapp-backend#:~:text=weatherapp%2Dbackend%2Dten%2Dnavy.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://vercel.com/totos-projects-883b9d76/weatherapp-frontend#:~:text=weatherapp%2Dfrontend%2Dolive.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
