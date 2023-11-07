892773f9811d2d05630ec5175749fab8a6fb8bca
TomB-S <147733386+TomB-S@users.noreply.github.com>
2023-11-07 09:49:06 +0100

    attente backend

--- urls.js
+++ urls.js
@@ -1,7 +1,9 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://vercel.com/totos-projects-883b9d76/weatherapp-backend#:~:text=weatherapp%2Dbackend%2Dten%2Dnavy.vercel.app';
+const BACKEND_URL = 'https://weatherapp-frontend-git-main-totos-projects-883b9d76.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://vercel.com/totos-projects-883b9d76/weatherapp-frontend#:~:text=weatherapp%2Dfrontend%2Dolive.vercel.app';
+const FRONTEND_URL = 'https://weatherapp-frontend-git-main-totos-projects-883b9d76.vercel.app/';
+
+
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
